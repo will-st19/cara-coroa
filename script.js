1577915e95dee5cdc82a22fa1959fcbefadba048
William Teixeira <william.teixeira@adp.com>
2023-11-09 14:16:25 -0300

    Adicionando contador

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+let cara = 0;
+let coroa = 0;
+
 const btnJogar = document.getElementById('btnJogar');
 const resultado = document.getElementById('resultado');
 
@@ -5,7 +8,12 @@ btnJogar.addEventListener('click', function () {
     const numero = Math.floor(Math.random() * 2);
     if (numero === 0) {
         resultado.innerHTML = '<h2>Cara!</h2><img src="heads.png">';
+        cara++;
     } else {
         resultado.innerHTML = '<h2>Coroa!</h2><img src="tails.png">';
+        coroa++;
     }
+
+    // Atualiza o contador
+    document.getElementById('contador').innerHTML = `Cara: ${cara} | Coroa: ${coroa}`;
 });
